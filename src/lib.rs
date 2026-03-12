e543bd6f99934ebe6f9c55648fe2ab85a2c079a0
Matthew Planchard <msplanchard@gmail.com>
2026-03-12 12:41:13 -0500

    Update lint configuration to use Cargo.toml specification
    Quite some time ago now, rust added the ability to read workspace
    lints from the `[workspace.lints]` property in the workspace's
    `Cargo.toml`. This mostly obviates the need for our custom script that
    inserts standard lints at the start of every entrypoint, but we've
    never gotten around to switching to it.
    
    This MR uses it as much as possible. The script to insert lints is
    still used to add a config-specific allow for printing in test code,
    since the global configuration cannot specify lints on a
    per-config-attr basis, but almost everything else has been moved into
    either the workspace Cargo.toml or individual overrides in packages'
    Cargo.tomls.
    
    This cleans up the top of all of our `lib.rs` and `main.rs` files
    considerably.
    
    Resolves RT-909
    
    Changelog: internal

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,17 @@
 // ::START CLIPPY LINTS::
-// This block is auto-generated. Please do not edit it directly! If you would
-// like to make changes, make them in `scripts/insert-clippy-lints.sh`.
 // -------------------------------------------------------------------
 // Non-default Lints
 // -------------------------------------------------------------------
-// Warn for any non-Send futures. These are fine for single-threaded
-// executors, but we use a multi-threaded one usually, and having
-// non-Send futures can lead to really obscure type errors
-#![deny(clippy::future_not_send)]
-// Deny direct comparison of floating point numbers
-#![deny(clippy::float_cmp)]
-// Deny direct comparison of const floating point numbers
-#![deny(clippy::float_cmp_const)]
-// Deny comparing floats for equality without using epsilon comparison
-#![deny(clippy::float_equality_without_abs)]
-// Deny defining float literals that are beyond the type's precision
-#![deny(clippy::lossy_float_literal)]
-// We need to be careful about printing anything to stdout/err in prod,
-// because it can be quite expensive in DD. All production output should
-// go through a logging macro, except potentially in cases where we
-// know it's not in a loop, e.g. service startup
-// Allow in debug builds, with a warning, but not in release builds
-#![cfg_attr(not(debug_assertions), deny(clippy::dbg_macro))]
-#![cfg_attr(not(debug_assertions), deny(clippy::print_stdout))]
-#![cfg_attr(not(debug_assertions), deny(clippy::print_stderr))]
-#![cfg_attr(debug_assertions, warn(clippy::dbg_macro))]
-#![cfg_attr(debug_assertions, warn(clippy::print_stdout))]
-#![cfg_attr(debug_assertions, warn(clippy::print_stderr))]
+// This block is auto-generated. Please do not edit it directly! If you would
+// like to make changes, either adjust the global lint settings in the
+// root-level Cargo.toml, override at your own package's Cargo.toml,
+// or, where you need build-level config, update in `scripts/insert-clippy-lints.sh`.
+// -------------------------------------------------------------------
+// Always allow printing in test code
 #![cfg_attr(test, allow(clippy::dbg_macro))]
 #![cfg_attr(test, allow(clippy::print_stdout))]
 #![cfg_attr(test, allow(clippy::print_stderr))]
-// Deny todo!() in release builds
-#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
-// But allow (with a warning so you remember it's there) in debug builds
-#![cfg_attr(debug_assertions, warn(clippy::todo))]
-// Warn on uninlined format args
-#![warn(clippy::uninlined_format_args)]
+// -------------------------------------------------------------------
 // ::END CLIPPY LINTS::
 pub mod conditions;
 
